6ce45894cfb4b9af4e5674c1c4803e09fde2fd19
Victor Fizesan <victorfizesan@gmail.com>
2025-03-04 15:54:48 +0000

    Correct gpt-2 initialisation

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ class CausalSelfAttention(nn.Module):
         assert config.n_embd % config.n_head == 0
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)  
         self.c_proj = nn.Linear(config.n_embd, config.n_embd) # project to attention residual
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         # Register buffer to store causal mask in model
@@ -47,6 +48,7 @@ class MLP(nn.Module):
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu    = nn.GELU(approximate='tanh')
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -90,6 +92,21 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        # Weight sharing of wte and lm_head
+        self.transformer.wte.weight = self.lm_head.weight
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):  
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
 
     # Inference to generate logits from token ID sequence input
     def forward(self, idx, targets=None):
@@ -163,6 +180,38 @@ class GPT(nn.Module):
 
 
 # ------------------------------------------------------------------------------------------------
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        with open('data/shakespeare/input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding ('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print (f"loaded {len (self. tokens) } tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        self. current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1]).view(B, T) 
+        y = (buf[1:]).view(B, T) 
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + (B * T + 1) > len(self.tokens) :
+            self.current_position = 0
+        return x, y
+
+
+
+# ------------------------------------------------------------------------------------------------
 
 device = 'cpu'
 if torch.cuda.is_available():
@@ -173,23 +222,19 @@ print("\n" + "="*40)
 print(f"Using device: {device}")
 print("="*40 + "\n")
 
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open('data/shakespeare/input.txt', 'r') as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1])
-buf = buf.to(device)
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+
+train_loader = DataLoaderLite(B=4, T=32) 
 
 model = GPT(GPTConfig())
 model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
